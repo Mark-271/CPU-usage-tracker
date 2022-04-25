8a99da8cb5adc05cd73cb14f8bf0422870563656
Mark Sungurov <mark.sungurov@gmail.com>
2022-04-26 00:01:34 +0300

    Integrate cpu_monitor module
    Signed-off-by: Mark Sungurov <mark.sungurov@gmail.com>

--- src/main.c
+++ src/main.c
@@ -7,5 +7,16 @@
 
 int main(void)
 {
+	const char *path = "/proc/stat";
+	cpu_monitor_init();
+
+	while(1) {
+		clear_screen();
+		cpu_monitor_read_data(path);
+		cpu_monitor_analyze_data();
+		sleep(1);
+	}
+
+	cpu_monitor_exit();
 	return EXIT_SUCCESS;
 }
